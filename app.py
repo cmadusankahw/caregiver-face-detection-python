2c3b1c2b1b85b45a50890016a3b95c241aad63dc
Chiran Hewawitharana <chiranh@zone24x7.com>
2020-10-30 16:41:08 +0530

    added user signup UI and logic

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
+from create_classifier import *
+from create_dataset import *
 from firebase_auth import *
+
+
+def center(self):
+    frameGm = self.frameGeometry()
+    screen = QtWidgets.QApplication.desktop().screenNumber(QtWidgets.QApplication.desktop().cursor().pos())
+    centerPoint = QtWidgets.QApplication.desktop().screenGeometry(screen).center()
+    frameGm.moveCenter(centerPoint)
+    self.move(frameGm.topLeft())
+
 
 class MainUi(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,24 +20,97 @@ class MainUi(QtWidgets.QMainWindow):
         uic.loadUi('home.ui', self)
 
         # Find the button
-        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton')
+        self.addNew = self.findChild(QtWidgets.QPushButton, 'addNewButton')
+        self.addNew.clicked.connect(self.showAddNewUi)
 
-        # Remember to pass the definition/method, not the return value!
-        self.button.clicked.connect(self.printButtonPressed)
+        self.centerWindow()
 
-        self.center()
-        self.show()
+    def centerWindow(self):
+        center(self)
 
-    def center(self):
-        frameGm = self.frameGeometry()
-        screen = QtWidgets.QApplication.desktop().screenNumber(QtWidgets.QApplication.desktop().cursor().pos())
-        centerPoint = QtWidgets.QApplication.desktop().screenGeometry(screen).center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
+    def showLoginUi(self):
+        self.registerElder = RegisterElderUi()
+        self.registerElder.show()
 
-    def printButtonPressed(self):
-        # This is executed when the button is pressed
-        print('printButtonPressed')
+
+# Add New Elder with Face Data Training ######################################################
+
+class RegisterElderUi(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(RegisterElderUi, self).__init__()
+        uic.loadUi('addElder.ui', self)
+
+        # Find the button
+        self.addElder = self.findChild(QtWidgets.QPushButton, 'addNewButton')
+        self.addElder.clicked.connect(self.addNewElder)
+
+        self.centerWindow()
+
+    def centerWindow(self):
+        center(self)
+
+    def addNewElder(self):
+        # add new elder
+        print('add new elder')
+
+    # ToDo to Edit
+    def capimg(self):
+        self.numimglabel.config(text=str("Captured Images = 0 "))
+        # messagebox.showinfo("INSTRUCTIONS", "We will Capture 300 pic of your Face.")
+        x = start_capture(self.controller.active_name)
+        self.controller.num_of_images = x
+        self.numimglabel.config(text=str("Number of images captured = " + str(x)))
+
+    # ToDo to edit
+    def trainmodel(self):
+        if self.controller.num_of_images < 300:
+            # messagebox.showerror("ERROR", "No enough Data, Capture at least 300 images!")
+            return
+        train_classifer(self.controller.active_name)
+        # messagebox.showinfo("SUCCESS", "The modele has been successfully trained!")
+        self.controller.show_frame("PageFour")
+
+
+# Login and Signup - CareGiver ################################################################
+
+class SignUpUi(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(SignUpUi, self).__init__()
+        uic.loadUi('signup.ui', self)
+
+        # Find the back to login button
+        self.signupButton = self.findChild(QtWidgets.QPushButton, 'loginButton')
+        self.signupButton.clicked.connect(self.showLoginUi)
+
+        # Find the signUp button
+        self.signButton = self.findChild(QtWidgets.QPushButton, 'signupButton')
+        self.signButton.clicked.connect(self.signupUser)
+
+        # email, password inputs
+        self.emailInput = self.findChild(QtWidgets.QLineEdit, 'emailInput')
+        self.passwordInput = self.findChild(QtWidgets.QLineEdit, 'passwordInput')
+
+        # find hint
+        self.hint = self.findChild(QtWidgets.QLabel, 'errorLabel')
+
+        self.centerWindow()
+        # self.show()
+
+    def centerWindow(self):
+        center(self)
+
+    def signupUser(self):
+        result = signUp(self.emailInput.text(), self.passwordInput.text())
+        if result == "success":
+            self.showLoginUi()
+        else:
+            self.hint.setProperty("text", result)
+
+    def showLoginUi(self):
+        SignUpUi.hide(self)
+        self.login = LoginUi()
+        self.login.show()
+
 
 class LoginUi(QtWidgets.QMainWindow):
     def __init__(self):
@@ -37,34 +121,36 @@ class LoginUi(QtWidgets.QMainWindow):
         self.loginButton = self.findChild(QtWidgets.QPushButton, 'loginButton')
         self.loginButton.clicked.connect(self.loginUser)
 
-        # Find the signUp button
+        # Find the oen signUp UI button
         self.signButton = self.findChild(QtWidgets.QPushButton, 'signupButton')
-        self.signButton.clicked.connect(self.signupUser)
+        self.signButton.clicked.connect(self.showSignUpUi)
 
         # email, password inputs
         self.emailInput = self.findChild(QtWidgets.QLineEdit, 'emailInput')
         self.passwordInput = self.findChild(QtWidgets.QLineEdit, 'passwordInput')
 
+        # find hint
+        self.hint = self.findChild(QtWidgets.QLabel, 'errorLabel')
 
-        self.center()
+        self.centerWindow()
         self.show()
 
-    def center(self):
-        frameGm = self.frameGeometry()
-        screen = QtWidgets.QApplication.desktop().screenNumber(QtWidgets.QApplication.desktop().cursor().pos())
-        centerPoint = QtWidgets.QApplication.desktop().screenGeometry(screen).center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
+    def centerWindow(self):
+        center(self)
 
     def loginUser(self):
         result = signIn(self.emailInput.text(), self.passwordInput.text())
         if result == "success":
             LoginUi.hide(self)
             self.home = MainUi()
-            self.home.show()
+            self.home.showFullScreen()
+        else:
+            self.hint.setProperty("text", result)
 
-    def signupUser(self):
-        signUp(self.emailInput.text(), self.passwordInput.text())
+    def showSignUpUi(self):
+        LoginUi.hide(self)
+        self.signup = SignUpUi()
+        self.signup.show()
 
 
 app = QtWidgets.QApplication(sys.argv)
